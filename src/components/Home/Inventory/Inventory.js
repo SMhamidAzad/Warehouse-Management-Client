234d6cde3de63810eec74a34a7715bc40a5a677d
SMhamidAzad <hamidazad020@gmail.com>
2022-05-01 16:40:53 +0600

    implement update quantity and delivered button

--- src/components/Home/Inventory/Inventory.js
+++ src/components/Home/Inventory/Inventory.js
@@ -15,12 +15,10 @@ const Inventory = () => {
 
     }, [id,isReload]);
 
-    const handleDelivered = () => {
-        const productQuantity = product.quantity;
-        const newQuantity=(productQuantity-1);
 
-        console.log(newQuantity);
+    const UpdateQuantity=(newQuantity)=>{
         const url = `http://localhost:5000/product/${id}`;
+       
         fetch(url, {
             method: 'PUT',
             headers: {
@@ -32,8 +30,22 @@ const Inventory = () => {
         .then(data =>{
             console.log(data);
             setIsReload(!isReload)
-            alert('successfully delivered!!!');
+            alert('successfully updated quantity!!!');
         })
+    }
+    const handleDelivered = () => {
+        const productQuantity = product.quantity;
+        const newQuantity=(productQuantity-1);
+
+        console.log(newQuantity);
+        UpdateQuantity(newQuantity);
+    }
+    const handleUpdateQuantity=e=>{
+        e.preventDefault();
+        const updateQuantity = parseInt(e.target.quantity.value);
+        console.log(typeof updateQuantity);
+
+        UpdateQuantity(updateQuantity)
     }
     return (
         <div className='container'>
@@ -48,6 +60,11 @@ const Inventory = () => {
                     <h4>Quantity: {product.quantity}</h4>
                     <h4>Price: {product.price}</h4>
                     <button onClick={() => handleDelivered()}>Delivered</button>
+                    <form onSubmit={handleUpdateQuantity}>
+                        <p>Do you want undate your product quantity?</p>
+                        <input type="text" name="quantity" id="" />
+                        <button>Update Quantity</button>
+                    </form>
                 </div>
             </div>
         </div>
