740fb1ee9c015c43beb1d4f7a3b9841c49eeb59c
SMhamidAzad <hamidazad020@gmail.com>
2022-05-05 17:45:26 +0600

    anage inventory table done with bootstrap react

--- src/components/Home/ManageSingleIn/ManageSingleIn.js
+++ src/components/Home/ManageSingleIn/ManageSingleIn.js
@@ -2,44 +2,49 @@ import React, { useEffect, useState } from 'react';
 import useReload from '../../hooks/useReload';
 import Products from '../Products/Products';
 
-const ManageSingleIn = ({product}) => {
-    const {_id,img, name, price, quantity}=product;
-    const [isReload,setIsReload]=useReload();
-   
+const ManageSingleIn = ({ product }) => {
+    const { _id, img, name, price, supplierName, quantity } = product;
+    const [isReload, setIsReload] = useReload();
+
     const [products, setProducts] = useState([])
     useEffect(() => {
         fetch('http://localhost:5000/product')
             .then(res => res.json())
             .then(data => setProducts(data))
     }, [isReload])
-    const handleDeleteBtn = id =>{
+    const handleDeleteBtn = id => {
         // console.log("deleted",id);
         const proceed = window.confirm('Are you sure you want to delete?');
-        if(proceed){
+        if (proceed) {
             console.log('deleting user with id, ', id);
             const url = `http://localhost:5000/product/${id}`;
             fetch(url, {
                 method: 'DELETE'
             })
-            .then(res => res.json())
-            .then(data =>{
-                if(data.deletedCount > 0){
-                    
-                    console.log('deleted');
-                    const remaining = products.filter(product => product._id !== id);
-                    setProducts(remaining);
-                    setIsReload(!isReload)
-                }
-            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+
+                        console.log('deleted');
+                        const remaining = products.filter(product => product._id !== id);
+                        setProducts(remaining);
+                        setIsReload(!isReload)
+                    }
+                })
         }
-       
+
     }
     return (
-        <div className='col'>
-            <img className='w-25' src={img} alt="" />
-            <p>{name}</p>
-            <button onClick={()=>handleDeleteBtn(_id)}>Delete</button>
-        </div>
+        <>
+            <tr>
+                <td>{name}</td>
+                <td>{price}</td>
+                <td>{quantity}</td>
+                {/* <td>{supplierName}</td> */}
+                <td><button className='bg-danger text-white border-0 rounded' onClick={() => handleDeleteBtn(_id)}>Delete</button></td>
+            </tr>
+        </>
+
     );
 };
 
--- src/components/Home/MangeInventory/ManageInventory.js
+++ src/components/Home/MangeInventory/ManageInventory.js
@@ -1,27 +1,42 @@
 import React, { useEffect, useState } from 'react';
+import { Table } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import useReload from '../../hooks/useReload';
 import ManageSingleIn from '../ManageSingleIn/ManageSingleIn';
 
 const ManageInventory = () => {
     const [products, setProducts] = useState([])
-    const [isReload,setIsReload]=useReload();
+    const [isReload, setIsReload] = useReload();
     useEffect(() => {
         fetch('http://localhost:5000/product')
             .then(res => res.json())
             .then(data => setProducts(data))
     }, [isReload])
     return (
-        <div className='row'>
-            <h2>This is manage inventory page</h2>
-            {
-                products.map(product => <ManageSingleIn
-                key={product._id}
-                product={product}
-                >
+        <div style={{backgroundColor: '#021B29'}} className='pb-5'>
+            <h2 className='text-center py-2 text-white'>Manage inventory</h2>
+            <Table style={{backgroundColor: '#021B29', color: 'white'}}  bordered  >
+                <thead>
+                    <tr>
+                        <th>Product Name</th>
+                        <th>Price</th>
+                        <th>Quantity</th>
+                        {/* <th>Supplier</th> */}
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>                  
+                   {
+                        products.map(product => <ManageSingleIn
+                            key={product._id}
+                            product={product}
+                        >
 
-                </ManageSingleIn>)
-            }
+                        </ManageSingleIn>)
+                    }
+                </tbody>
+            </Table>
+
             <Link to='/addItem'>Add New Inventory</Link>
         </div>
     );
