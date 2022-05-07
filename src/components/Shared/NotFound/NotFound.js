22941ccd8afdf2f1119aa73b20b7aed6f8ce8c52
SMhamidAzad <hamidazad020@gmail.com>
2022-05-07 08:01:13 +0600

    updated readme file

--- src/components/Shared/NotFound/NotFound.js
+++ src/components/Shared/NotFound/NotFound.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { BsArrowRight } from 'react-icons/bs';
 
 const NotFound = () => {
     return (
         <div style={{ height: '90vh' }} className='text-center pt-5'>
             <h1 className=' pt-5'>Oops the page you are looking is not found!!</h1>
             <h1 className='text-danger fw-bold'>404!!</h1>
-            <Link to='/' className='primary-btn'>Back To Home</Link>
+            <Link to='/' className='primary-btn'>
+                Back To Home
+                <BsArrowRight></BsArrowRight>
+            </Link>
         </div>
     );
 };
